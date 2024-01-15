8896007d4683d0e676b308c8d75e1e5a3c972c51
gerald-wambui <geraldwainaina25@gmail.com>
2024-01-15 20:34:10 +0300

    initial

---
+++ core-data/src/main/java/com/jaguh/pokedex/core/data/MainActivity.kt
@@ -0,0 +1,10 @@
+package com.jaguh.pokedex.core.data
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+	}
+}
