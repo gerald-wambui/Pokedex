023f5e21033867b6ae1fbb9a96fef4424f4a3e89
gerald-wambui <geraldwainaina25@gmail.com>
2024-01-16 08:23:37 +0300

    gradle

--- core-data/build.gradle.kts
+++ core-data/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
 	namespace = "com.jaguh.pokedex.core.data"
-	compileSdk = 33
+	compileSdk = 34
 
 	defaultConfig {
 		applicationId = "com.jaguh.pokedex.core.data"
 		minSdk = 24
-		targetSdk = 33
+		targetSdk = 34
 		versionCode = 1
 		versionName = "1.0"
 
