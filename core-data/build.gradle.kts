46c52e28292bad5bceffe70eb3369f0c7a228a43
gerald-wambui <geraldwainaina25@gmail.com>
2024-01-16 09:02:14 +0300

    resources

--- core-data/build.gradle.kts
+++ core-data/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
 	id("com.android.application")
 	id("org.jetbrains.kotlin.android")
+	//kotlin("ksp")
+	//id("com.google.devtools.ksp")
 }
 
 android {
@@ -37,10 +39,18 @@ android {
 
 dependencies {
 
+
 	implementation("androidx.core:core-ktx:1.9.0")
 	implementation("androidx.appcompat:appcompat:1.6.1")
 	implementation("com.google.android.material:material:1.11.0")
 	testImplementation("junit:junit:4.13.2")
 	androidTestImplementation("androidx.test.ext:junit:1.1.5")
 	androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+	//
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+	testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
+
+	implementation("com.google.dagger:hilt-android:2.48.1")
+	//ksp("com.google.dagger:hilt-android-compiler:2.48.1")
 }
