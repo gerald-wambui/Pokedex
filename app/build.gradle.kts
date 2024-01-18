22abf9cb2edffe58b1fda6efb780cc78d7be8504
gerald-wambui <geraldwainaina25@gmail.com>
2024-01-18 21:25:05 +0300

    basics

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
 	id("com.android.application")
 	id("org.jetbrains.kotlin.android")
 	kotlin("kapt")
+	id("kotlin-kapt")
 	id("com.google.dagger.hilt.android")
 }
 
@@ -17,6 +18,10 @@ android {
 		versionName = "1.0"
 
 		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+	}
+
+	buildFeatures {
+		dataBinding = true
 	}
 
 	buildTypes {
@@ -43,13 +48,29 @@ dependencies {
 	implementation("androidx.appcompat:appcompat:1.6.1")
 	implementation("com.google.android.material:material:1.11.0")
 	implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+	implementation("com.android.databinding:library:8.2.1")
 	testImplementation("junit:junit:4.13.2")
 	androidTestImplementation("androidx.test.ext:junit:1.1.5")
 	androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
 	implementation("com.github.skydoves:transformationlayout:1.1.3")
 	implementation("com.github.skydoves:progressview:1.1.3")
+	implementation("com.github.skydoves:androidribbon:1.0.4")
 
-	implementation("com.google.dagger:hilt-android:2.44")
-	kapt("com.google.dagger:hilt-android-compiler:2.44")
+	implementation("com.google.dagger:hilt-android:2.48.1")
+	kapt("com.google.dagger:hilt-android-compiler:2.48.1")
+
+	implementation("com.jakewharton.timber:timber:5.0.1")
+
+	 // Check for latest version
+
+	// ViewModel and viewModelScope
+	implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:8.2.1")
+
+	// LiveData
+	implementation("androidx.lifecycle:lifecycle-livedata-ktx:8.2.1")
+
+	// Data binding
+	implementation("androidx.databinding:databinding-runtime:8.2.1")
+	kapt("androidx.databinding:databinding-compiler:8.2.1")
 }
--- app/src/main/java/com/jaguh/pokedex/ui/details/DetailViewModel.kt
+++ app/src/main/java/com/jaguh/pokedex/ui/details/DetailViewModel.kt
@@ -1,4 +1,52 @@
 package com.jaguh.pokedex.ui.details
 
-class DetailViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedInject
+import kotlinx.coroutines.flow.Flow
+
+class DetailViewModel @AssistedInject constructor(
+	detailRepository: DetailRepository,
+	@Assisted private val pokemonName: String,
+): BindingViewModel(){
+
+	@get:Bindable
+	val isLoading: Boolean by bindingProperty(true)
+		private set
+
+	@get:Bindable
+	val toastMessage: String? by bindingProperty(null)
+		private set
+
+	private val pokemonInfoFlow: Flow<PokemonInfo?> = detailRepository.fetchPokemonInfo(
+		name = pokemonName,
+		onComplete = { isLoading = false },
+		onError = { toastMessage = it },
+	)
+
+	@get:Bindable
+	val pokemonInfo: PokemonInfo by pokemonInfoFlow.asBindingProperty(viewModelScope, null)
+
+	init {
+		Timber.d("init DetailViewModel")
+	}
+
+	@dagger.assisted.AssistedFactory
+	interface AssistedFactory {
+		fun create(pokemonName: String): DetailViewModel
+	}
+
+	companion object{
+		fun provideFactory(
+			assistedFactory: AssistedFactory,
+			pokemonName: String,
+		): ViewModelProvider.Factory = object : ViewModelProvider.Factory{
+
+			@Suppress("UNCHECKED_CAST")
+			override fun <T : ViewModel> create(modelClass: Class<T>): T {
+				return assistedFactory.create(pokemonName) as T
+			}
+		}
+	}
 }
