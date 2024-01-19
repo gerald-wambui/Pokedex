72c3b1560f94a04064f1136b014b4854e5888c4f
gerald-wambui <geraldwainaina25@gmail.com>
2024-01-19 12:10:19 +0300

    build configuration files

---
+++ buildSrc/Configuration.kt
@@ -0,0 +1,12 @@
+package com.jaguh.pokedex
+
+object Configuration {
+	const val compileSdk = 34
+	const val targetSdk = 34
+	const val minSdk = 23
+	const val majorVersion = 1
+	const val minorVersion = 1
+	const val patchVersion = 2
+	const val versionName = "$majorVersion.$minorVersion.$patchVersion"
+	const val versionCode = 10
+}
