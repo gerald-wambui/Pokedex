f716bb0d7e95f520394158a672dee94c5912c8b2
gerald-wambui <geraldwainaina25@gmail.com>
2024-01-20 07:23:09 +0300

    tests

--- app/src/test/java/com/jaguh/pokedex/DetailViewModelTest.kt
+++ app/src/test/java/com/jaguh/pokedex/DetailViewModelTest.kt
@@ -1,1 +1,11 @@
-package com.jaguh.poke
+package com.jaguh.pokedex
+
+
+class DetailViewModelTest {
+
+	private lateinit var viewModel: DetailViewModel
+	private lateinit var detailRepository: DetailRepository
+	private val pokedexService: PokedexService = mock()
+	private val pokedexClient: PokedexClient = PokedexClient
+	private val pokemonInfoDao: PokemonInfoDao = mock()
+}
