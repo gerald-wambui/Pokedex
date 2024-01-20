cf54eca287e52d14a90e5c18d71526290d9f760f
gerald-wambui <geraldwainaina25@gmail.com>
2024-01-20 08:13:17 +0300

    pokemon

--- core-model/src/main/java/com/jaguh/pokedex/core/model/Pokemon.kt
+++ core-model/src/main/java/com/jaguh/pokedex/core/model/Pokemon.kt
@@ -1,2 +1,21 @@
 package com.jaguh.pokedex.core.model
 
+import android.os.Parcelable
+import com.squareup.moshi.JsonClass
+
+
+@Parcelize
+@JsonClass(generateAdapter = true)
+data class Pokemon(
+	var page: Int = 0,
+	@field:Json(name = "name")
+	val name: String,
+	@field:Json(name = "url") val url: String,
+): Parcelable {
+
+	fun getImageUrl(): String {
+		val index = url.split("/".toRegex()).dropLast(1).last()
+		return "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/" +
+				"pokemon/other/official-artwork/$index.png"
+	}
+}
